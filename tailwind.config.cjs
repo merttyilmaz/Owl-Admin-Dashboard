3bb96cf9396fd3d4da79540095a2e3c6c8bc25cf
Mert Yılmaz <myyilmaz97@gmail.com>
2022-12-07 18:42:34 +0300

    Initialized the project with vite, ts, tailwind, mui-icons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        2: "2 2 0%",
+        3: "3 3 0%",
+        4: "4 4 0%",
+        5: "5 5 0%",
+        6: "6 6 0%",
+        7: "7 7 0%",
+        8: "8 8 0%",
+        9: "9 9 0%",
+        10: "10 10 0%",
+      },
+    },
   },
   plugins: [],
 };
